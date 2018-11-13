A2P3-runs; with embeddings + POS predicts "1" for all reviews. - Fixed to produce the same-length vectors for test data as for training data. - Added argmax computation over predicted class probabilities.

assignment_2/p3/p3_tf_MLP.py
@@ -98,11 +98,12 @@ def get_data(input_type, num_cross_validation_trials):
     vocabulary_size = len(vocabulary)
     print("Training vocabulary size: %d" % vocabulary_size)
     print("Number of training reviews: %d" % len(review_words))
+    print("Length of review_data vectors: %d" % len(review_data[0]))
 
     # Compile reviews, with word index encoding
     #... for the test data ...
     test_reviews, test_words, test_review_words, test_review_data = \
-        p3_utils.load_test_set(test_text, dictionary)
+        p3_utils.load_test_set(test_text, dictionary, VECTOR_LEN=len(review_data[0]))
     print("Number of test reviews: %d" % len(test_review_words))
 
     # Load word vectors if going to use them
@@ -118,7 +119,8 @@ def get_data(input_type, num_cross_validation_trials):
     # Get POS vectors if going to use them
     if input_type == 'pos' or input_type == 'awv+pos':
         review_pos_vectors = p3_utils.get_pos_tags_reviews(train_text)
-        test_pos_vectors = p3_utils.get_pos_tags_reviews(test_text, HAS_RATINGS=False)
+        test_pos_vectors = p3_utils.get_pos_tags_reviews(test_text, \
+            HAS_RATINGS=False, VECTOR_LEN=len(review_pos_vectors[0]))
 
     # Prepare combined word vectors and pos vectors if going to use them
     if input_type == 'awv+pos':
@@ -203,7 +205,7 @@ def get_data(input_type, num_cross_validation_trials):
         p3_utils.split_training_data_for_cross_validation(train_data, review_labels, \
             num_cross_validation_trials)
 
-    return xval_sets, test_data
+    return xval_sets, test_reviews, test_data
 
 def run_one_trial(train_data, train_labels, val_data, val_labels, num_epochs_per_trial, \
         num_h1_units, h1_activation, h2_activation, h1_h2_dropout_rate):
@@ -335,7 +337,7 @@ if __name__ == '__main__':
 
     num_epochs_for_training = 20    # ... when training on full training set
 
-    xval_sets, test_data = get_data(input_type, num_cross_validation_trials)
+    xval_sets, test_reviews, test_data = get_data(input_type, num_cross_validation_trials)
 
     nowStr = datetime.now().strftime("%B %d, %Y %I:%M:%S %p")
     print("====" + nowStr + "====")
@@ -360,6 +362,7 @@ if __name__ == '__main__':
     np_test_data = np.array(test_data)
     print("Test data shape: %s" % str(np_test_data.shape))
     test_labels = model.predict(np_test_data)
+    test_ratings = [ np.argmax(predictions) for predictions in test_labels  ]
     print("Test labels shape: %s" % str(test_labels.shape))
     print("Test labels: %s" % test_labels[:5])
 
@@ -370,7 +373,7 @@ if __name__ == '__main__':
     outFilename = "test_data_%s_%d-%s_%d-%s_%d-%s" % \
         (input_type, num_h1_units, h1_activation, 10, h2_activation, 5, "SOFTMAX")
     p3_utils.write_test_set_with_ratings(outDir, outFilename, \
-        test_reviews, test_labels)
+        test_reviews, test_ratings)
 
     nowStr = datetime.now().strftime("%B %d, %Y %I:%M:%S %p")
     print("====" + nowStr + "====")

assignment_2/p3/p3_utils.py
@@ -88,11 +88,14 @@ def compile_vocabulary(words, vocabulary_size):
         reverse_dictionary[iWord] = word
     return fd_words, vocabulary, dictionary, reverse_dictionary
 
-def get_review_data(review_words, dictionary):
+def get_review_data(review_words, dictionary, VECTOR_LEN=None):
     # compile review data as the indices of the words into the vocabulary
     review_data = []
-    max_len = max(len(review) for review in review_words)
-    len_review_vector = ((max_len + 9) // 10) * 10  # multiple of 10
+    if VECTOR_LEN == None:
+        max_len = max(len(review) for review in review_words)
+        len_review_vector = ((max_len + 9) // 10) * 10  # multiple of 10
+    else:
+        len_review_vector = VECTOR_LEN
     for review in review_words:
         words_in_review = [0] * len_review_vector
         for i, word in enumerate(review):
@@ -135,7 +138,7 @@ def get_words_and_ratings(text):
     return words, review_words, review_data, review_labels, \
         fd_words, vocabulary, dictionary, reverse_dictionary
 
-def load_test_set(text, dictionary):
+def load_test_set(text, dictionary, VECTOR_LEN):
     """
     Get the words and vocabulary from the reviews in a test sest.
     The test set has one review sentence per line and does not have any ratings.
@@ -146,7 +149,7 @@ def load_test_set(text, dictionary):
     # tokenize the review sentences and compile a list of words
     review_words = [ review.split() for review in reviews ]
     words = [ word for words_in_sent in review_words for word in words_in_sent ]
-    review_data = get_review_data(review_words, dictionary)
+    review_data = get_review_data(review_words, dictionary, VECTOR_LEN)
     # return the test set reviews
     return reviews, words, review_words, review_data
 
@@ -308,7 +311,7 @@ TAGSET = {
 tags_dictionary = { list(TAGSET)[i] : i for i in range(len(TAGSET)) }
 reverse_tags_dictionary = { i: list(TAGSET)[i] for i in range(len(TAGSET)) }
 
-def get_pos_tags_reviews(text, HAS_RATINGS=True):
+def get_pos_tags_reviews(text, HAS_RATINGS=True, VECTOR_LEN=None):
     """
     Get a vector of POS tags for each review sentence.
     Return -
@@ -327,8 +330,11 @@ def get_pos_tags_reviews(text, HAS_RATINGS=True):
     review_pos_tags_w_caps = [ nltk.pos_tag(s) for s in review_words_w_caps ]
     _reviews_tag_vectors = [ [ tags_dictionary.get(tag, 0) for word, tag in s ] \
         for s in review_pos_tags_w_caps ]
-    max_len = max(len(s) for s in _reviews_tag_vectors)
-    vector_len = ((max_len + 9) // 10) * 10
+    if VECTOR_LEN == None:
+        max_len = max(len(s) for s in _reviews_tag_vectors)
+        vector_len = ((max_len + 9) // 10) * 10
+    else:
+        vector_len = VECTOR_LEN
     reviews_tag_vectors = [ s + [PAD] * (vector_len - len(s))\
         for s in _reviews_tag_vectors ]
     return np.array(reviews_tag_vectors)
@@ -421,7 +427,7 @@ def load_sentiment_vectors(review_words):
     return review_sentiment_vectors
 
 # ------------------------------------------------------------------------
-# Transform review representation ---
+# Hot Vectors ---
 # ------------------------------------------------------------------------
 
 def one_hot_vectors(review_data, vocabulary_size):