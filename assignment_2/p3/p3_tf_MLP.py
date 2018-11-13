A2P3-add POS vector, pass labels to plot, load test data and predict - attempt to correct inputs to model.predict   + BUG NOT FIXED: test input wrong shape, different from trained model   + Must use training data vector length for test data

assignment_2/p3/p3_tf_MLP.py
@@ -20,7 +20,7 @@ from tensorflow.python.keras.layers import Dropout
 
 import p3_utils
 
-def mlp_model(input_shape, h1_units, h1_activation='relu', h2_activation='relu', \
+def mlp_model(input_shape, num_h1_units, h1_activation='relu', h2_activation='relu', \
               num_classes=5, dropout_rate=0.0, input_dropout_rate=0.0):
     """
     Creates a TF Keras Multi-Layer Perceptron model,
@@ -29,15 +29,15 @@ def mlp_model(input_shape, h1_units, h1_activation='relu', h2_activation='relu',
     input_dim = input_shape[0]
     print("--- Model ---")
     print("Input    : %d x %d" % (1, input_dim))
-    print("Layer h1 : %d x %d %s" % (input_dim, h1_units, h1_activation))
-    print("Layer h2 : %d x %d %s" % (h1_units, 10, h2_activation))
+    print("Layer h1 : %d x %d %s" % (input_dim, num_h1_units, h1_activation))
+    print("Layer h2 : %d x %d %s" % (num_h1_units, 10, h2_activation))
     print("Output   : %d x %d SOFTMAX" % (10, 5))
     print("----------")
     model = models.Sequential()
     # input layer
     model.add(Dropout(rate=input_dropout_rate, input_shape=input_shape))
     # h1: hidden layer 1
-    model.add(Dense(units=h1_units, activation=h1_activation))
+    model.add(Dense(units=num_h1_units, activation=h1_activation))
     model.add(Dropout(rate=dropout_rate))
     # h2: hidden layer 2
     model.add(Dense(units=10, activation=h2_activation))
@@ -46,13 +46,13 @@ def mlp_model(input_shape, h1_units, h1_activation='relu', h2_activation='relu',
     model.add(Dense(units=num_classes, activation='softmax'))
     return model
 
-def mlp_train(model, data, epochs=10):
+def mlp_train(model, train_data, epochs=10):
     """
     Train the Multi-Layer Perceptron.
     Inputs:
-        data - training data and labels, evaluation data and labels
+        train_data - training data and labels, evaluation data and labels
     """
-    (train_data, train_labels), (val_data, val_labels) = data
+    (train_data, train_labels), (val_data, val_labels) = train_data
 
     if len(val_data) == 0 or len(val_labels) == 0:
         validation_data = None
@@ -72,76 +72,138 @@ def mlp_train(model, data, epochs=10):
 
     return history
 
-def get_data(filePath, input_type, num_cross_validation_trials):
+def get_data(input_type, num_cross_validation_trials):
     """
     Load the data and select the correct format.
     Return -
 
     """
-    # Load the text
-    text = p3_utils.get_text_from_file(filePath)
-    print("Read %d bytes from '%s' as text" % (len(text), filePath))
-    print("Text begins : '%s'" % text[:30])
+    # Load the training text
+    train_text = p3_utils.get_text_from_file(p3_utils.PATH_TRAIN)
+    print("Read %d bytes from '%s' as text" % \
+        (len(train_text), p3_utils.PATH_TRAIN))
+    print("Text begins : '%s'" % train_text[:30])
+
+    # Load the test text
+    test_text = p3_utils.get_text_from_file(p3_utils.PATH_TEST)
+    print("Read %d bytes from '%s' as text" % \
+        (len(test_text), p3_utils.PATH_TEST))
+    print("Text begins : '%s'" % test_text[:30])
 
     # Compile reviews and ratings, with word index encoding
+    # ... for the training data ...
     words, review_words, review_data, review_labels, \
         fd_words, vocabulary, dictionary, reverse_dictionary = \
-        p3_utils.get_words_and_ratings(text)
+        p3_utils.get_words_and_ratings(train_text)
     vocabulary_size = len(vocabulary)
-    print("Vocabulary size: %d" % vocabulary_size)
-    print("Number of reviews: %d" % len(review_words))
+    print("Training vocabulary size: %d" % vocabulary_size)
+    print("Number of training reviews: %d" % len(review_words))
+
+    # Compile reviews, with word index encoding
+    #... for the test data ...
+    test_reviews, test_words, test_review_words, test_review_data = \
+        p3_utils.load_test_set(test_text, dictionary)
+    print("Number of test reviews: %d" % len(test_review_words))
 
     # Load word vectors if going to use them
-    if input_type == 'avg wv' or input_type == 'awv+sv':
-        vectors, wv_dictionary, wv_reverse_dictionary, \
-            wv_review_data, wv_review_vectors, vw_review_sentence_average_vectors \
-            = p3_utils.load_embeddings_gensim(fd_words, review_words)
+    if input_type == 'avg wv' or input_type == 'awv+sv' or input_type == 'awv+pos':
+        vectors = p3_utils.load_embeddings_gensim()
+        wv_vocabulary, wv_dictionary, wv_reverse_dictionary \
+            = p3_utils.get_embeddings_vocabulary(vectors, fd_words)
+        wv_review_data, wv_review_vectors, vw_review_sentence_average_vectors \
+            = p3_utils.get_embeddings(vectors, review_words)
+        wv_test_data, wv_test_vectors, vw_test_sentence_average_vectors \
+            = p3_utils.get_embeddings(vectors, test_review_words)
+
+    # Get POS vectors if going to use them
+    if input_type == 'pos' or input_type == 'awv+pos':
+        review_pos_vectors = p3_utils.get_pos_tags_reviews(train_text)
+        test_pos_vectors = p3_utils.get_pos_tags_reviews(test_text, HAS_RATINGS=False)
+
+    # Prepare combined word vectors and pos vectors if going to use them
+    if input_type == 'awv+pos':
+        # ... for training data ...
+        review_sentence_awv_pos = []
+        for i in range(len(review_words)):
+            rsv = np.concatenate([vw_review_sentence_average_vectors[i], \
+                                  review_pos_vectors[i]])
+            review_sentence_awv_pos.append(rsv)
+        # ... for test data ...
+        test_sentence_awv_pos = []
+        for i in range(len(test_review_words)):
+            rsv = np.concatenate([vw_test_sentence_average_vectors[i], \
+                                  test_pos_vectors[i]])
+            test_sentence_awv_pos.append(rsv)
 
     # Load sentiment vectors if going to use them
     if input_type == 'rsv' or input_type == 'awv+sv':
         review_sentiment_vectors = p3_utils.load_sentiment_vectors(review_words)
+        test_sentiment_vectors = p3_utils.load_sentiment_vectors(test_review_words)
 
     # Prepare combined word vectors and sentiment vectors if going to use them
     if input_type == 'awv+sv':
+        # ... for training data ...
         review_sentence_awv_sv = []
         for i in range(len(review_words)):
             rsv = np.concatenate([vw_review_sentence_average_vectors[i], \
                                   review_sentiment_vectors[i]])
             review_sentence_awv_sv.append(rsv)
+        # ... for test data ...
+        test_sentence_awv_sv = []
+        for i in range(len(test_review_words)):
+            rsv = np.concatenate([vw_test_sentence_average_vectors[i], \
+                                  test_sentiment_vectors[i]])
+            test_sentence_awv_sv.append(rsv)
 
     # Select the input type
     if input_type == 'one hot':
-        data = p3_utils.one_hot_vectors(review_data, vocabulary_size)
-        print("Count one-hot vectors: %d" % len(data))
+        train_data = p3_utils.one_hot_vectors(review_data, vocabulary_size)
+        test_data = p3_utils.one_hot_vectors(test_data, vocabulary_size)
+        print("Count one-hot vectors: %d" % len(train_data))
     elif input_type == 'count-hot':
-        data = p3_utils.count_vectors(review_data, vocabulary_size)
-        print("Count count vectors: %d" % len(data))
+        train_data = p3_utils.count_vectors(review_data, vocabulary_size)
+        test_data = p3_utils.count_vectors(test_data, vocabulary_size)
+        print("Count count vectors: %d" % len(train_data))
     elif input_type == 'td-idf hot':
-        data = p3_utils.tdIdf_vectors(review_data, vocabulary_size)
-        print("Count tdIdf vectors: %d" % len(data))
+        train_data = p3_utils.tdIdf_vectors(review_data, vocabulary_size)
+        test_data = p3_utils.tdIdf_vectors(test_data, vocabulary_size)
+        print("Count tdIdf vectors: %d" % len(train_data))
     elif input_type == 'word index':
-        data = review_data
-        print("Count word index vectors: %d" % len(data))
+        train_data = review_data
+        test_data = test_data
+        print("Count word index vectors: %d" % len(train_data))
     elif input_type == 'avg wv':
-        data = vw_review_sentence_average_vectors
-        print("Count embedding vectors: %d" % len(data))
+        train_data = vw_review_sentence_average_vectors
+        test_data = vw_test_sentence_average_vectors
+        print("Count embedding vectors: %d" % len(train_data))
     elif input_type == 'rsv':
-        data = review_sentiment_vectors
-        print("Count sentiment vectors: %d" % len(data))
+        train_data = review_sentiment_vectors
+        test_data = test_sentiment_vectors
+        print("Count sentiment vectors: %d" % len(train_data))
     elif input_type == 'awv+sv':
-        data = review_sentence_awv_sv
-        print("Count embedding + sentiment vectors: %d" % len(data))
+        train_data = review_sentence_awv_sv
+        test_data = test_sentence_awv_sv
+        print("Count embedding + sentiment vectors: %d" % len(train_data))
+    elif input_type == 'pos':
+        train_data = review_pos_vectors
+        test_data = test_pos_vectors
+        print("Count POS vectors: %d" % len(train_data))
+    elif input_type == 'awv+pos':
+        train_data = review_sentence_awv_pos
+        test_data = test_sentence_awv_pos
+        print("Count embedding + POS vectors: %d" % len(train_data))
     else:
         raise InvalidArgumentException("Unrecognized data input type: %s" % input_type)
 
-    assert(len(data) == len(review_words))
-    assert(len(data) == len(review_labels))
+    assert(len(train_data) == len(review_words))
+    assert(len(train_data) == len(review_labels))
+    assert(len(test_data) == len(test_review_words))
 
     shuffle_indices, xval_sets = \
-        p3_utils.split_training_data_for_cross_validation(data, review_labels, \
+        p3_utils.split_training_data_for_cross_validation(train_data, review_labels, \
             num_cross_validation_trials)
 
-    return xval_sets
+    return xval_sets, test_data
 
 def run_one_trial(train_data, train_labels, val_data, val_labels, num_epochs_per_trial, \
         num_h1_units, h1_activation, h2_activation, h1_h2_dropout_rate):
@@ -150,21 +212,21 @@ def run_one_trial(train_data, train_labels, val_data, val_labels, num_epochs_per
     np_train_labels = np.array(train_labels)
     np_val_data = np.array(val_data)
     np_val_labels = np.array(val_labels)
-    data = ((np_train_data, np_train_labels), (np_val_data, np_val_labels))
+    train_data = ((np_train_data, np_train_labels), (np_val_data, np_val_labels))
 
     nowStr = datetime.now().strftime("%B %d, %Y %I:%M:%S %p")
     print("====" + nowStr + "====")
 
     model = mlp_model(input_shape=np_train_data.shape[1:], \
-                      h1_units=num_h1_units, h1_activation=h1_activation, \
+                      num_h1_units=num_h1_units, h1_activation=h1_activation, \
                       h2_activation=h2_activation, dropout_rate=h1_h2_dropout_rate)
 
     nowStr = datetime.now().strftime("%B %d, %Y %I:%M:%S %p")
     print("====" + nowStr + "====")
 
-    history = mlp_train(model, data, epochs=num_epochs_per_trial)
+    history = mlp_train(model, train_data, epochs=num_epochs_per_trial)
 
-    return history
+    return model, history
 
 def run_trials(xval_sets, num_cross_validation_trials, num_epochs_per_trial, \
         num_h1_units, h1_activation, h2_activation, h1_h2_dropout_rate):
@@ -180,7 +242,8 @@ def run_trials(xval_sets, num_cross_validation_trials, num_epochs_per_trial, \
         (train_data, train_labels), (val_data, val_labels) = \
             p3_utils.assemble_cross_validation_data(xval_sets, iTrial)
 
-        history = run_one_trial(train_data, train_labels, val_data, val_labels, num_epochs_per_trial, \
+        model, history = \
+            run_one_trial(train_data, train_labels, val_data, val_labels, num_epochs_per_trial, \
                 num_h1_units, h1_activation, h2_activation, h1_h2_dropout_rate)
 
         scores.append(history)
@@ -190,7 +253,9 @@ def run_trials(xval_sets, num_cross_validation_trials, num_epochs_per_trial, \
 
     return scores
 
-def print_results_of_trials(scores, num_epochs_per_trial):
+def print_results_of_trials(scores, trial_parameters):
+
+    input_type, num_h1_units, h1_activation, h2_activation, num_epochs_per_trial = trial_parameters
 
     print(" Trial Results ".center(80, '='))
     for iTrial, history in enumerate(scores):
@@ -238,11 +303,15 @@ def print_results_of_trials(scores, num_epochs_per_trial):
         val_acc_min = val_acc_mean = val_acc_max = None
 
     # Plot loss and accuracy over the trials
+    heading = "Keras MLP: %s:Lin, %d:%s, 10:%s, 5:Softmax; epochs=%d" % \
+        (input_type, num_h1_units, h1_activation, h2_activation, num_epochs_per_trial)
+    if val_acc_min != None and val_acc_mean != None and val_acc_max != None:
+        subheading = "validation accuracy: %7.4f %7.4f %7.4f" % \
+            (val_acc_min, val_acc_mean, val_acc_max)
+    else:
+        subheading = ""
     p3_utils.plot_results(np_train_loss, np_train_acc, np_val_loss, np_val_acc, \
-        val_acc_min, val_acc_mean, val_acc_max, \
-        input_type=input_type, h1_units=num_h1_units, \
-        h1_f=h1_activation, h2_f=h2_activation, \
-        epochs=num_epochs_per_trial)
+        heading, subheading)
 
 # ------------------------------------------------------------------------
 # Tests ---
@@ -256,7 +325,7 @@ if __name__ == '__main__':
     print("====" + nowStr + "====")
 
     # Set parameters for this set of trials
-    input_type = 'rsv'
+    input_type = 'awv+pos'
     num_cross_validation_trials = 10
     num_epochs_per_trial = 40
     num_h1_units = 60
@@ -266,21 +335,42 @@ if __name__ == '__main__':
 
     num_epochs_for_training = 20    # ... when training on full training set
 
-    filePath = "data/a2_p3_train_data.txt"
-    xval_sets = get_data(filePath, input_type, num_cross_validation_trials)
+    xval_sets, test_data = get_data(input_type, num_cross_validation_trials)
 
     nowStr = datetime.now().strftime("%B %d, %Y %I:%M:%S %p")
     print("====" + nowStr + "====")
 
+    trial_parameters = (input_type, num_h1_units, \
+        h1_activation, h2_activation, num_epochs_for_training)
+
     scores = run_trials(xval_sets, num_cross_validation_trials, num_epochs_per_trial, \
             num_h1_units, h1_activation, h2_activation, h1_h2_dropout_rate)
-    print_results_of_trials(scores, num_epochs_per_trial)
+    print_results_of_trials(scores, trial_parameters)
 
     train_data, train_labels = p3_utils.assemble_full_training_data(xval_sets)
 
-    history = run_one_trial(train_data, train_labels, [], [], num_epochs_for_training, \
+    model, history = \
+        run_one_trial(train_data, train_labels, [], [], num_epochs_for_training, \
             num_h1_units, h1_activation, h2_activation, h1_h2_dropout_rate)
-    print_results_of_trials([ history ], num_epochs_for_training)
+    print_results_of_trials([ history ], trial_parameters)
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    model.save("data/p3_tf_MLP_model_" + timestamp + ".h5")
+
+    np_test_data = np.array(test_data)
+    print("Test data shape: %s" % str(np_test_data.shape))
+    test_labels = model.predict(np_test_data)
+    print("Test labels shape: %s" % str(test_labels.shape))
+    print("Test labels: %s" % test_labels[:5])
+
+    nowStr = datetime.now().strftime("%B %d, %Y %I:%M:%S %p")
+    print("====" + nowStr + "====")
+
+    outDir = "tests"
+    outFilename = "test_data_%s_%d-%s_%d-%s_%d-%s" % \
+        (input_type, num_h1_units, h1_activation, 10, h2_activation, 5, "SOFTMAX")
+    p3_utils.write_test_set_with_ratings(outDir, outFilename, \
+        test_reviews, test_labels)
 
     nowStr = datetime.now().strftime("%B %d, %Y %I:%M:%S %p")
     print("====" + nowStr + "====")

assignment_2/p3/p3_utils.py
@@ -12,9 +12,10 @@ import math
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import nltk
+
 from datetime import datetime
 from struct import unpack
-from nltk import FreqDist
 from gensim.models import KeyedVectors
 
 PATH_TRAIN = "data/a2_p3_train_data.txt"
@@ -74,7 +75,7 @@ def get_text_from_file(filePath):
 
 def compile_vocabulary(words, vocabulary_size):
     # compile vocabulary, with a UNK entry at index 0 for the words left out
-    fd_words = FreqDist(words)
+    fd_words = nltk.FreqDist(words)
     frequents = fd_words.most_common(VOCABULARY_SIZE)
     count_UNK = len(fd_words) - len(frequents)
     frequents = [( 'UNK', count_UNK )] + frequents
@@ -288,6 +289,49 @@ def get_embeddings(vectors, words_in_sents):
         for s in words_in_sents ]
     wv_sentence_average_vectors = [ np.mean(s, axis=0) for s in wv_vectors ]
     return wv_data, wv_vectors, wv_sentence_average_vectors
+
+# ------------------------------------------------------------------------
+# POS tagging ---
+# ------------------------------------------------------------------------
+
+# tagset from nltk.help.upenn_tagset(), plus:
+#   '#', which occurs in the training data
+#   'UNK', in case the tagger returns something not in this tag set
+TAGSET = {
+        'UNK', '$', "''", '(', ')', ',', '--', '.', ':', 'CC', 'CD', \
+        'DT', 'EX', 'FW', 'IN', 'JJ', 'JJR', 'JJS', 'LS', 'MD', \
+        'NN', 'NNP', 'NNPS', 'NNS', 'PDT', 'POS', 'PRP', 'PRP$', \
+        'RB', 'RBR', 'RBS', 'RP', 'SYM', 'TO', 'UH', 'VB', 'VBD', \
+        'VBG', 'VBN', 'VBP', 'VBZ', 'WDT', 'WP', 'WP$', 'WRB', \
+        '``', '#'
+    }
+tags_dictionary = { list(TAGSET)[i] : i for i in range(len(TAGSET)) }
+reverse_tags_dictionary = { i: list(TAGSET)[i] for i in range(len(TAGSET)) }
+
+def get_pos_tags_reviews(text, HAS_RATINGS=True):
+    """
+    Get a vector of POS tags for each review sentence.
+    Return -
+        reviews_tag_vectors - [ [ tag index, tag index, ... ] ... ]
+    """
+    PAD = -1
+    if (HAS_RATINGS):
+        re_reviews = re.compile(r'^(.+)\|(.+)$', re.MULTILINE)
+    else:
+        re_reviews = re.compile(r'^(.+)$', re.MULTILINE)
+    reviews_w_caps = re_reviews.findall(text)
+    if (HAS_RATINGS):
+        review_words_w_caps = [ review.split() for review, rating in reviews_w_caps ]
+    else:
+        review_words_w_caps = [ review.split() for review in reviews_w_caps ]
+    review_pos_tags_w_caps = [ nltk.pos_tag(s) for s in review_words_w_caps ]
+    _reviews_tag_vectors = [ [ tags_dictionary.get(tag, 0) for word, tag in s ] \
+        for s in review_pos_tags_w_caps ]
+    max_len = max(len(s) for s in _reviews_tag_vectors)
+    vector_len = ((max_len + 9) // 10) * 10
+    reviews_tag_vectors = [ s + [PAD] * (vector_len - len(s))\
+        for s in _reviews_tag_vectors ]
+    return np.array(reviews_tag_vectors)
 
 # ------------------------------------------------------------------------
 # Sentiment Lexicons ---
@@ -617,17 +661,12 @@ def assemble_full_training_data(xval_sets):
 # ------------------------------------------------------------------------
 
 def plot_results(np_train_loss, np_train_acc, np_val_loss, np_val_acc, \
-        val_acc_min, val_acc_mean, val_acc_max, \
-        input_type, h1_units, h1_f, h2_f, epochs, \
-        plotName='tests/p3_tf_MLP_test_plot_'):
+        heading, subheading, plotName='tests/p3_tf_MLP_test_plot_'):
 
     figure, axis_1 = plt.subplots()
 
-    plt.suptitle("Keras MLP: %s:Lin, %d:%s, 10:%s, 5:Softmax; epochs=%d" % \
-        (input_type, h1_units, h1_f, h2_f, epochs), size=14)
-    if val_acc_min != None and val_acc_mean != None and val_acc_max != None:
-        plt.title("validation accuracy: %7.4f %7.4f %7.4f" % \
-            (val_acc_min, val_acc_mean, val_acc_max), size=12)
+    plt.suptitle(heading, size=12)
+    plt.title(subheading, size=10)
 
     # Plot loss for both training and validation
     axis_1.plot(np_train_loss, 'r--')